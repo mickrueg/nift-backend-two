768d0bf6f9f592f92d049f371e77131c2e79db40
mickrueg <mickrueg@gmail.com>
2022-08-25 15:17:45 -0600

    Fixed ADD article

--- models/Article.js
+++ models/Article.js
@@ -4,7 +4,9 @@ const mongoose = require('../db/connection')
 // Schema(s)
 const ArticleSchema = new mongoose.Schema({
     user: String,
-    article: String
+    article: String,
+    date: String,
+    title: String
 })
 
 // Instantiate the model and give it a name
